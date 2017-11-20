d5633d211c5c0f1f6af66a39db37fe78f225613a
galileoguzman <galileoguzman@gmail.com>
2017-11-20 09:14:02 -0600

    Initial Commit

---
+++ eJackson/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  eJackson
+//
+//  Created by Galileo Guzman on 11/20/17.
+//  Copyright © 2017 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ eJackson/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  eJackson
+//
+//  Created by Galileo Guzman on 11/20/17.
+//  Copyright © 2017 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
